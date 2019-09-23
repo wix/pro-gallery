31c333aac0e6a9a651f297cb0f5e9ffa841e49d6
Travis CI User <travis@example.org>
2019-09-23 08:21:45 +0000

    Updates

---
+++ precache-manifest.8163d01ef17c35227ead18ab13be4b1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4b5fccfc7d0f250bf60388efcfef23",
+    "url": "/pro-gallery/index.html"
+  },
+  {
+    "revision": "d4097931ade6b499c795",
+    "url": "/pro-gallery/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "55a9c50b6a1fa386d8a1",
+    "url": "/pro-gallery/static/css/main.d4a48427.chunk.css"
+  },
+  {
+    "revision": "d4097931ade6b499c795",
+    "url": "/pro-gallery/static/js/2.d050f4a1.chunk.js"
+  },
+  {
+    "revision": "55a9c50b6a1fa386d8a1",
+    "url": "/pro-gallery/static/js/main.4734d903.chunk.js"
+  },
+  {
+    "revision": "cc47ca826ce581b02dd9",
+    "url": "/pro-gallery/static/js/runtime~main.0f1f2266.js"
+  },
+  {
+    "revision": "6703d899ec96fa2ca0e0bf8bc840a5e7",
+    "url": "/pro-gallery/static/media/progallery-svg-font-icons.6703d899.6703d899.eot"
+  },
+  {
+    "revision": "76bb6acaa7638521d8388b47961eef2c",
+    "url": "/pro-gallery/static/media/progallery-svg-font-icons.76bb6aca.76bb6aca.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pro-gallery/precache-manifest.f6693c7b7b9d0b0ef7d68c65fabfbecf.js"
+  "/pro-gallery/precache-manifest.8163d01ef17c35227ead18ab13be4b1d.js"
 );
 
 self.addEventListener('message', (event) => {
