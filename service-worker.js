dfc1ebda87632ec66242831b9e3b668359e10bd2
Travis CI User <travis@example.org>
2019-10-07 10:47:00 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pro-gallery/precache-manifest.5553c63ad2dfff8d0e80d6a6fe6a7036.js"
+  "/pro-gallery/precache-manifest.c8f298b897bd4f19dc94aa54225f406a.js"
 );
 
 self.addEventListener('message', (event) => {
